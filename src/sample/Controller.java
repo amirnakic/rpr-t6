f75dc64f8b7e3a4a3e9cd6602747e6c5833412af
amirnakic <anakic1@etf.unsa.ba>
2018-11-26 18:43:45 +0100

    Odgovarajućim poljima dodan je bidirectionalbind.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,13 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class Controller {
+    private SimpleStringProperty username;
+
     public TextField nameField;
     public TextField surnameField;
     public TextField indexField;
@@ -19,6 +23,21 @@ public class Controller {
     public ChoiceBox statusBox;
     public ChoiceBox categoryBox;
     public Button confirmButton;
+
+    public Controller() {
+        username = new SimpleStringProperty("");
+    }
+
+    @FXML
+    public void initialize() {
+        nameField.textProperty().bindBidirectional(username);
+        surnameField.textProperty().bindBidirectional(username);
+        indexField.textProperty().bindBidirectional(username);
+        jmbgField.textProperty().bindBidirectional(username);
+        adressField.textProperty().bindBidirectional(username);
+        telephoneField.textProperty().bindBidirectional(username);
+        eMailField.textProperty().bindBidirectional(username);
+    }
 
     public void clickOnConfirm(ActionEvent actionEvent) {
     }
