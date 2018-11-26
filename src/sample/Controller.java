95ab04997283ea5862c6b9034d03437d6d40d43e
amirnakic <anakic1@etf.unsa.ba>
2018-11-26 18:24:02 +0100

    Dodani su atributi za svako polje/dugme unutar klase Controller.java.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class Controller {
+    public TextField nameField;
+    public TextField surnameField;
+    public TextField indexField;
+    public TextField jmbgField;
+    public DatePicker dateField;
+    public ComboBox placeOfBirthField;
+    public TextField adressField;
+    public TextField telephoneField;
+    public TextField eMailField;
+    public ChoiceBox departmentBox;
+    public ChoiceBox yearBox;
+    public ChoiceBox cycleBox;
+    public ChoiceBox statusBox;
+    public ChoiceBox categoryBox;
+    public Button confirmButton;
+
+    public void clickOnConfirm(ActionEvent actionEvent) {
+    }
 }
