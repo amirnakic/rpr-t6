0314c060e751e1eb9e5e810f38c9543b4b3937ca
amirnakic <anakic1@etf.unsa.ba>
2018-11-26 19:44:18 +0100

    Dodane su validacije polja za unos osnovnih podataka o studentu.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,12 @@
 package sample;
 
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class Controller {
-    private SimpleStringProperty username;
 
     public TextField nameField;
     public TextField surnameField;
@@ -23,20 +23,87 @@ public class Controller {
     public ChoiceBox statusBox;
     public ChoiceBox categoryBox;
     public Button confirmButton;
+    public boolean imeValidno;
+    public boolean prezimeValidno;
+    public boolean indeksValidan;
 
-    public Controller() {
-        username = new SimpleStringProperty("");
+    public boolean validnoIme(String s) {
+        if (s.length() > 20 || s.length() < 1) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!(Character.isLetter(c) || c==' ')) return false;
+        }
+        return true;
+    }
+
+    public boolean validnoPrezime(String s) {
+        if (s.length() > 20 || s.length() < 1) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!(Character.isLetter(c) || c=='-')) return false;
+        }
+        return true;
+    }
+
+    public boolean validanIndeks(String s) {
+        if (s.length() != 5) return false;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
     }
 
     @FXML
     public void initialize() {
-        nameField.textProperty().bindBidirectional(username);
-        surnameField.textProperty().bindBidirectional(username);
-        indexField.textProperty().bindBidirectional(username);
-        jmbgField.textProperty().bindBidirectional(username);
-        adressField.textProperty().bindBidirectional(username);
-        telephoneField.textProperty().bindBidirectional(username);
-        eMailField.textProperty().bindBidirectional(username);
+        imeValidno = false;
+        nameField.getStyleClass().add("poljeNeispravno");
+        nameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validnoIme(n)) {
+                    nameField.getStyleClass().removeAll("poljeNeispravno");
+                    nameField.getStyleClass().add("poljeIspravno");
+                    imeValidno = true;
+                } else {
+                    nameField.getStyleClass().removeAll("poljeIspravno");
+                    nameField.getStyleClass().add("poljeNeispravno");
+                    imeValidno = false;
+                }
+            }
+        });
+        prezimeValidno = false;
+        surnameField.getStyleClass().add("poljeNeispravno");
+        surnameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validnoPrezime(n)) {
+                    surnameField.getStyleClass().removeAll("poljeNeispravno");
+                    surnameField.getStyleClass().add("poljeIspravno");
+                    prezimeValidno = true;
+                } else {
+                    surnameField.getStyleClass().removeAll("poljeIspravno");
+                    surnameField.getStyleClass().add("poljeNeispravno");
+                    prezimeValidno = false;
+                }
+            }
+        });
+        indeksValidan = false;
+        indexField.getStyleClass().add("poljeNeispravno");
+        indexField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (validanIndeks(n)) {
+                    indexField.getStyleClass().removeAll("poljeNeispravno");
+                    indexField.getStyleClass().add("poljeIspravno");
+                    indeksValidan = true;
+                } else {
+                    indexField.getStyleClass().removeAll("poljeIspravno");
+                    indexField.getStyleClass().add("poljeNeispravno");
+                    indeksValidan = false;
+                }
+            }
+        });
     }
 
     public void clickOnConfirm(ActionEvent actionEvent) {
